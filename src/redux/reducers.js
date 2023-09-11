999734db49604e6429a16096df288c4cc224a1c5
pravesh meena <pravesh.meena2892@gmail.com>
2023-09-11 08:24:44 +0530

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,13 +3,35 @@ const initialState = {
 };
 
 const todoReducer = (state = initialState, action) => {
-  switch (
-    action.type
-    //Write a code as per your need
-
-    // default:
-    //   return state;
-  ) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: action.id,
+            text: action.text,
+            completed: false,
+          },
+        ],
+      };
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    default:
+      return state;
   }
 };
 
